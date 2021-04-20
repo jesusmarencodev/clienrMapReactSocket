89e19e25f245412a3ddbdde40ddb692ef7bd00c3
Jesus Marenco <jesusmarencodev@gmail.com>
2021-04-20 17:43:22 -0500

    agregar sockets

--- src/MapsApp.js
+++ src/MapsApp.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { SocketProvider } from './context/SocketContext'
 import { MapPage } from './pages/MapPage'
 
 export const MapsApp = () => {
   return (
-    <div>
+    <SocketProvider>
       <MapPage/>
-    </div>
+    </SocketProvider>
   )
 }
--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { SocketContext } from '../context/SocketContext';
 
 import { useMapbox } from '../hooks/useMapbox';
 
@@ -13,17 +14,29 @@ export const MapPage = () => {
 
   const { coords, setRef, nuevoMarcador$, movimientoMarcador$ } = useMapbox(puntoInicial);
 
+  const { socket } = useContext(SocketContext);
+
+  //nuevo marcador
   useEffect(() => {
     nuevoMarcador$.subscribe(marcador => {
-      console.log(marcador)
+      socket.emit('marcador-nuevo', marcador);
     })
-  }, [nuevoMarcador$]);
+  }, [nuevoMarcador$, socket]);
 
+  //movimientos de marcador
   useEffect(() => {
     movimientoMarcador$.subscribe(marcador => {
       console.log(marcador.id)
     })
   }, [movimientoMarcador$]);
+
+
+  //escuchar nuevos marcadores
+  useEffect(() => {
+    socket.on('marcador-nuevo', (marcador)=>{
+      console.log(marcador)
+    });
+  }, [socket]);
 
 
   return (
